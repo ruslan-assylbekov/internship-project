d0c06af7a0623360ef5b6425a505d497a1095916
ruslan-assylbekov <captainberkut@gmail.com>
2026-06-08 14:26:42 +0500

    added weather display for any city

--- main.py
+++ main.py
@@ -1,14 +1,29 @@
 from fastapi import FastAPI
 import requests
+from fastapi.responses import FileResponse
 
 app = FastAPI()
 
 API_KEY = "0acc9d47af6d4897955121059261003"
 
-
 @app.get("/")
-def root():
-    return {"Heloworld"}
+def homepage():
+    return FileResponse("index.html")
+
+# @app.get("/")
+# def root():
+#     return {"Heloworld"}
+
+@app.get("/weather")
+def get_weather():
+    url = "http://api.weatherapi.com/v1/current.json"
+    params = {
+        "key": API_KEY,
+        "q": "London"
+    }
+    response = requests.get(url, params=params)
+
+    return response.json()
 
 
 @app.get("/weather/{city}")
@@ -20,6 +35,15 @@ def get_weather(city: str):
     }
     response = requests.get(url, params=params)
 
-    return response.json()
+    data = response.json()
+
+    return {
+        "city": data["location"]["name"],
+        "country": data["location"]["country"],
+        "temperature_c": data["current"]["temp_c"],
+        "condition": data["current"]["condition"]["text"],
+        "humidity": data["current"]["humidity"]
+    }
 
 
+#uvicorn main:app --reload
